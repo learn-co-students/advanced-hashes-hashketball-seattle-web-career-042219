91c82011fec32730791e6c4393f2b3a85ae34bc0
Sean Watters <sean@h2os.org>
2019-04-17 23:23:43 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,171 @@
 # Write your code here!
+def game_hash
+ game_hash = {
+  :home => {
+    :team_name => "Brooklyn Nets",
+    :colors => ["Black", "White"],
+    :players => {
+      "Alan Anderson" => {
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1
+      },
+      "Reggie Evans" => {
+          :number => 30,
+          :shoe => 14,
+          :points => 12,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 12,
+          :blocks => 12,
+          :slam_dunks => 7
+      },
+      "Brook Lopez" => {
+          :number => 11,
+          :shoe => 17,
+          :points => 17,
+          :rebounds => 19,
+          :assists => 10,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 15
+      },
+      "Mason Plumlee" => {
+          :number => 1,
+          :shoe => 19,
+          :points => 26,
+          :rebounds => 12,
+          :assists => 6,
+          :steals => 3,
+          :blocks => 8,
+          :slam_dunks => 5
+      },
+      "Jason Terry" => {
+          :number => 31,
+          :shoe => 15,
+          :points => 19,
+          :rebounds => 2,
+          :assists => 2,
+          :steals => 4,
+          :blocks => 11,
+          :slam_dunks => 1
+      }
+    }
+  },
+
+  :away => {
+    :team_name => "Charlotte Hornets",
+    :colors => ["Turquoise", "Purple"],
+    :players => {
+      "Jeff Adrien" => {
+          :number => 4,
+          :shoe => 18,
+          :points => 10,
+          :rebounds => 1,
+          :assists => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 2
+      },
+      "Bismak Biyombo" => {
+          :number => 0,
+          :shoe => 16,
+          :points => 12,
+          :rebounds => 4,
+          :assists => 7,
+          :steals => 7,
+          :blocks => 15,
+          :slam_dunks => 10
+      },
+      "DeSagna Diop" => {
+          :number => 2,
+          :shoe => 14,
+          :points => 24,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 4,
+          :blocks => 5,
+          :slam_dunks => 5
+      },
+      "Ben Gordon" => {
+          :number => 8,
+          :shoe => 15,
+          :points => 33,
+          :rebounds => 3,
+          :assists => 2,
+          :steals => 1,
+          :blocks => 1,
+          :slam_dunks => 0
+      },
+      "Brendan Haywood" => {
+          :number => 33,
+          :shoe => 15,
+          :points => 6,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 22,
+          :blocks => 5,
+          :slam_dunks => 12
+        }
+      }
+    }
+  }
+end
+
+def num_points_scored(player_name)
+  players = {}
+
+  game_hash.each {|key, team| players.merge!(team[:players])}
+
+  players_with_name = players.select {|player, data| player == player_name}
+  return players_with_name[player_name][:points]
+end
+
+def shoe_size(player_name)
+  players = {}
+
+  game_hash.each {|key, team| players.merge!(team[:players])}
+
+  players_with_name = players.select {|player, data| player == player_name}
+  return players_with_name[player_name][:shoe]
+end
+
+def team_colors(team)
+  team_colors = {}
+  game_hash.each{|key, information| team_colors.merge!(information.values[0] => information.values[1])}
+  return team_colors[team]
+end
+
+def team_names
+  return game_hash.collect{|key, team| team.values[0]}
+end
+
+def player_numbers(team_name)
+  return game_hash.select {|key, team| team[:team_name] == team_name}.values[0][:players].collect {|name, info| info[:number]}
+end
+
+def player_stats(player_name)
+  players = {}
+
+  game_hash.each {|key, team| players.merge!(team[:players])}
+
+  players_with_name = players.select {|player, data| player == player_name}
+  return players_with_name[player_name]
+end
+
+def big_shoe_rebounds
+  players = {}
+
+  game_hash.each {|key, team| players.merge!(team[:players])}
+
+  shoe_size = game_hash.collect {|key, team| team[:players].collect {|name, info| info[:shoe]}}.flatten.max
+
+  return players.select {|player, data| data[:shoe] == shoe_size}.values[0][:rebounds]
 
 
-
-
-
-
-
-
-
-
+end
